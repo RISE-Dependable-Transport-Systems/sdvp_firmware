d26f20ec059bf68af6dd456665bdad1b574c9470
Marvin Damschen <marvin.damschen@ri.se>
2020-08-14 16:11:51 +0200

    commands: prepare copter integration

--- common/commands.c
+++ common/commands.c
@@ -105,6 +105,7 @@ void commands_process_packet(unsigned char *data, unsigned int len,
 			id_ret = ID_CAR_CLIENT;
 		}
 
+		// General commands
 		switch (packet_id) {
 		case CMD_HEARTBEAT: {
 			timeout_reset();
@@ -607,101 +608,177 @@ void commands_process_packet(unsigned char *data, unsigned int len,
 			commands_send_packet(m_send_buffer, send_index);
 		} break;
 
-		case CMD_GET_STATE: {
-			POS_STATE pos;
-			float accel[3];
-			float gyro[3];
-			float mag[3];
-			const mc_values mcval = bldc_interface_get_last_received_values();
-			ROUTE_POINT rp_goal;
+		default:
+			break;
+		}
 
-			commands_set_send_func(func);
+		// Rover-specific commands
+		if (VEHICLE_TYPE == VEHICLE_TYPE_ROVER) {
+			switch(packet_id) {
+			case CMD_GET_STATE: {
+				POS_STATE pos;
+				float accel[3];
+				float gyro[3];
+				float mag[3];
+				const mc_values mcval = bldc_interface_get_last_received_values();
+				ROUTE_POINT rp_goal;
 
-			pos_get(&pos);
-			pos_imu_get(accel, gyro, mag);
-			autopilot_get_goal_now(&rp_goal);
+				commands_set_send_func(func);
 
-			int32_t send_index = 0;
-			m_send_buffer[send_index++] = id_ret; // 1
-			m_send_buffer[send_index++] = CMD_GET_STATE; // 2
-			m_send_buffer[send_index++] = FW_VERSION_MAJOR; // 3
-			m_send_buffer[send_index++] = FW_VERSION_MINOR; // 4
-			buffer_append_float32(m_send_buffer, pos.roll, 1e6, &send_index); // 8
-			buffer_append_float32(m_send_buffer, pos.pitch, 1e6, &send_index); // 12
-			buffer_append_float32(m_send_buffer, pos.yaw, 1e6, &send_index); // 16
-			buffer_append_float32(m_send_buffer, accel[0], 1e6, &send_index); // 20
-			buffer_append_float32(m_send_buffer, accel[1], 1e6, &send_index); // 24
-			buffer_append_float32(m_send_buffer, accel[2], 1e6, &send_index); // 28
-			buffer_append_float32(m_send_buffer, gyro[0], 1e6, &send_index); // 32
-			buffer_append_float32(m_send_buffer, gyro[1], 1e6, &send_index); // 36
-			buffer_append_float32(m_send_buffer, gyro[2], 1e6, &send_index); // 40
-			buffer_append_float32(m_send_buffer, mag[0], 1e6, &send_index); // 44
-			buffer_append_float32(m_send_buffer, mag[1], 1e6, &send_index); // 48
-			buffer_append_float32(m_send_buffer, mag[2], 1e6, &send_index); // 52
-			buffer_append_float32(m_send_buffer, pos.px, 1e4, &send_index); // 56
-			buffer_append_float32(m_send_buffer, pos.py, 1e4, &send_index); // 60
-			buffer_append_float32(m_send_buffer, pos.speed, 1e6, &send_index); // 64
-			buffer_append_float32(m_send_buffer, mcval.v_in, 1e6, &send_index); // 68
-			buffer_append_float32(m_send_buffer, mcval.temp_mos, 1e6, &send_index); // 72
-			m_send_buffer[send_index++] = mcval.fault_code; // 73
-			buffer_append_float32(m_send_buffer, pos.px_gps, 1e4, &send_index); // 77
-			buffer_append_float32(m_send_buffer, pos.py_gps, 1e4, &send_index); // 81
-			buffer_append_float32(m_send_buffer, rp_goal.px, 1e4, &send_index); // 85
-			buffer_append_float32(m_send_buffer, rp_goal.py, 1e4, &send_index); // 89
-			buffer_append_float32(m_send_buffer, autopilot_get_rad_now(), 1e6, &send_index); // 93
-			buffer_append_int32(m_send_buffer, time_today_get_ms(), &send_index); // 97
-			buffer_append_int16(m_send_buffer, autopilot_get_route_left(), &send_index); // 99
-			buffer_append_float32(m_send_buffer, pos.px, 1e4, &send_index); // 103
-			buffer_append_float32(m_send_buffer, pos.py, 1e4, &send_index); // 107
+				pos_get(&pos);
+				pos_imu_get(accel, gyro, mag);
+				autopilot_get_goal_now(&rp_goal);
 
-			commands_send_packet(m_send_buffer, send_index);
-		} break;
+				int32_t send_index = 0;
+				m_send_buffer[send_index++] = id_ret; // 1
+				m_send_buffer[send_index++] = CMD_GET_STATE; // 2
+				m_send_buffer[send_index++] = FW_VERSION_MAJOR; // 3
+				m_send_buffer[send_index++] = FW_VERSION_MINOR; // 4
+				buffer_append_float32(m_send_buffer, pos.roll, 1e6, &send_index); // 8
+				buffer_append_float32(m_send_buffer, pos.pitch, 1e6, &send_index); // 12
+				buffer_append_float32(m_send_buffer, pos.yaw, 1e6, &send_index); // 16
+				buffer_append_float32(m_send_buffer, accel[0], 1e6, &send_index); // 20
+				buffer_append_float32(m_send_buffer, accel[1], 1e6, &send_index); // 24
+				buffer_append_float32(m_send_buffer, accel[2], 1e6, &send_index); // 28
+				buffer_append_float32(m_send_buffer, gyro[0], 1e6, &send_index); // 32
+				buffer_append_float32(m_send_buffer, gyro[1], 1e6, &send_index); // 36
+				buffer_append_float32(m_send_buffer, gyro[2], 1e6, &send_index); // 40
+				buffer_append_float32(m_send_buffer, mag[0], 1e6, &send_index); // 44
+				buffer_append_float32(m_send_buffer, mag[1], 1e6, &send_index); // 48
+				buffer_append_float32(m_send_buffer, mag[2], 1e6, &send_index); // 52
+				buffer_append_float32(m_send_buffer, pos.px, 1e4, &send_index); // 56
+				buffer_append_float32(m_send_buffer, pos.py, 1e4, &send_index); // 60
+				buffer_append_float32(m_send_buffer, pos.speed, 1e6, &send_index); // 64
+				buffer_append_float32(m_send_buffer, mcval.v_in, 1e6, &send_index); // 68
+				buffer_append_float32(m_send_buffer, mcval.temp_mos, 1e6, &send_index); // 72
+				m_send_buffer[send_index++] = mcval.fault_code; // 73
+				buffer_append_float32(m_send_buffer, pos.px_gps, 1e4, &send_index); // 77
+				buffer_append_float32(m_send_buffer, pos.py_gps, 1e4, &send_index); // 81
+				buffer_append_float32(m_send_buffer, rp_goal.px, 1e4, &send_index); // 85
+				buffer_append_float32(m_send_buffer, rp_goal.py, 1e4, &send_index); // 89
+				buffer_append_float32(m_send_buffer, autopilot_get_rad_now(), 1e6, &send_index); // 93
+				buffer_append_int32(m_send_buffer, time_today_get_ms(), &send_index); // 97
+				buffer_append_int16(m_send_buffer, autopilot_get_route_left(), &send_index); // 99
+				buffer_append_float32(m_send_buffer, pos.px, 1e4, &send_index); // 103
+				buffer_append_float32(m_send_buffer, pos.py, 1e4, &send_index); // 107
 
-		case CMD_RC_CONTROL: {
-			RC_MODE mode;
-			float throttle, steering;
-			int32_t ind = 0;
-			mode = data[ind++];
-			throttle = buffer_get_float32(data, 1e4, &ind);
-			steering = buffer_get_float32(data, 1e6, &ind);
+				commands_send_packet(m_send_buffer, send_index);
+			} break;
 
-			utils_truncate_number(&steering, -1.0, 1.0);
-			//steering *= autopilot_get_steering_scale();
+			case CMD_RC_CONTROL: {
+				RC_MODE mode;
+				float throttle, steering;
+				int32_t ind = 0;
+				mode = data[ind++];
+				throttle = buffer_get_float32(data, 1e4, &ind);
+				steering = buffer_get_float32(data, 1e6, &ind);
 
-			//autopilot_set_active(false);
+				utils_truncate_number(&steering, -1.0, 1.0);
+				//steering *= autopilot_get_steering_scale();
 
-			switch (mode) {
-			case RC_MODE_CURRENT:
-				if (!main_config.car.disable_motor) {
-					comm_can_set_vesc_id(VESC_ID);
-					bldc_interface_set_current(throttle);
+				//autopilot_set_active(false);
+
+				switch (mode) {
+				case RC_MODE_CURRENT:
+					if (!main_config.car.disable_motor) {
+						comm_can_set_vesc_id(VESC_ID);
+						bldc_interface_set_current(throttle);
+					}
+					break;
+				case RC_MODE_DUTY:
+					utils_truncate_number(&throttle, -1.0, 1.0);
+					if (!main_config.car.disable_motor) {
+						comm_can_set_vesc_id(VESC_ID);
+						bldc_interface_set_duty_cycle(throttle);
+					}
+					break;
+				default:
+					break;
 				}
-				break;
-			case RC_MODE_DUTY:
-				utils_truncate_number(&throttle, -1.0, 1.0);
-				if (!main_config.car.disable_motor) {
-					comm_can_set_vesc_id(VESC_ID);
-					bldc_interface_set_duty_cycle(throttle);
-				}
-				break;
+				steering = utils_map(steering, -1.0, 1.0,
+						main_config.car.steering_center + (main_config.car.steering_range / 2.0),
+						main_config.car.steering_center - (main_config.car.steering_range / 2.0));
+				servo_pwm_set_ramped(0, steering);
+			} break;
+
+			case CMD_SET_SERVO_DIRECT: {
+				int32_t ind = 0;
+				float steering = buffer_get_float32(data, 1e6, &ind);
+				utils_truncate_number(&steering, 0.0, 1.0);
+				servo_pwm_set_ramped(0, steering);
+			} break;
+
 			default:
 				break;
 			}
-			steering = utils_map(steering, -1.0, 1.0,
-					main_config.car.steering_center + (main_config.car.steering_range / 2.0),
-					main_config.car.steering_center - (main_config.car.steering_range / 2.0));
-			servo_pwm_set_ramped(0, steering);
-		} break;
+		}
 
-		case CMD_SET_SERVO_DIRECT: {
-			int32_t ind = 0;
-			float steering = buffer_get_float32(data, 1e6, &ind);
-			utils_truncate_number(&steering, 0.0, 1.0);
-			servo_pwm_set_ramped(0, steering);
-		} break;
+		// Copter-specific commands
+		if (VEHICLE_TYPE == VEHICLE_TYPE_COPTER) {
+			switch(packet_id) {
+			case CMD_MR_GET_STATE: {
+				POS_STATE pos;
+				float accel[3];
+				float gyro[3];
+				float mag[3];
+				ROUTE_POINT rp_goal;
 
-		default:
-			break;
+				commands_set_send_func(func);
+
+				pos_imu_get(accel, gyro, mag);
+				pos_get(&pos);
+				autopilot_get_goal_now(&rp_goal);
+
+				int32_t send_index = 0;
+				m_send_buffer[send_index++] = id_ret; // 1
+				m_send_buffer[send_index++] = CMD_MR_GET_STATE; // 2
+				m_send_buffer[send_index++] = FW_VERSION_MAJOR; // 3
+				m_send_buffer[send_index++] = FW_VERSION_MINOR; // 4
+				buffer_append_float32_auto(m_send_buffer, pos.roll, &send_index); // 8
+				buffer_append_float32_auto(m_send_buffer, pos.pitch, &send_index); // 12
+				buffer_append_float32_auto(m_send_buffer, pos.yaw, &send_index); // 16
+				buffer_append_float32_auto(m_send_buffer, accel[0], &send_index); // 20
+				buffer_append_float32_auto(m_send_buffer, accel[1], &send_index); // 24
+				buffer_append_float32_auto(m_send_buffer, accel[2], &send_index); // 28
+				buffer_append_float32_auto(m_send_buffer, gyro[0], &send_index); // 32
+				buffer_append_float32_auto(m_send_buffer, gyro[1], &send_index); // 36
+				buffer_append_float32_auto(m_send_buffer, gyro[2], &send_index); // 40
+				buffer_append_float32_auto(m_send_buffer, mag[0], &send_index); // 44
+				buffer_append_float32_auto(m_send_buffer, mag[1], &send_index); // 48
+				buffer_append_float32_auto(m_send_buffer, mag[2], &send_index); // 52
+				buffer_append_float32_auto(m_send_buffer, pos.px, &send_index); // 56
+				buffer_append_float32_auto(m_send_buffer, pos.py, &send_index); // 60
+				buffer_append_float32_auto(m_send_buffer, pos.pz, &send_index); // 64
+				buffer_append_float32_auto(m_send_buffer, pos.speed, &send_index); // 68
+				buffer_append_float32_auto(m_send_buffer, 0.0, &send_index); // 72
+				buffer_append_float32_auto(m_send_buffer, pos.px_gps, &send_index); // 76
+				buffer_append_float32_auto(m_send_buffer, pos.py_gps, &send_index); // 80
+				buffer_append_float32_auto(m_send_buffer, rp_goal.px, &send_index); // 84
+				buffer_append_float32_auto(m_send_buffer, rp_goal.py, &send_index); // 88
+				buffer_append_int32(m_send_buffer, time_today_get_ms(), &send_index); // 92
+				commands_send_packet(m_send_buffer, send_index);
+			} break;
+
+//			case CMD_MR_RC_CONTROL: {
+//				int32_t ind = 0;
+//				float throttle = buffer_get_float32_auto(data, &ind);
+//				float roll = buffer_get_float32_auto(data, &ind);
+//				float pitch = buffer_get_float32_auto(data, &ind);
+//				float yaw = buffer_get_float32_auto(data, &ind);
+//				copter_control_set_input(throttle, roll, pitch, yaw);
+//			} break;
+//
+//			case CMD_MR_OVERRIDE_POWER: {
+//				int32_t ind = 0;
+//				copter_control_set_motor_override(0, buffer_get_float32_auto(data, &ind));
+//				copter_control_set_motor_override(1, buffer_get_float32_auto(data, &ind));
+//				copter_control_set_motor_override(2, buffer_get_float32_auto(data, &ind));
+//				copter_control_set_motor_override(3, buffer_get_float32_auto(data, &ind));
+//			} break;
+
+			default:
+				break;
+			}
 		}
 	}
 }
